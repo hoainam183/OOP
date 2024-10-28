379610a5a133982d9b6bc2e4d07755d8e143a4e8
Hoai Nam <nhnnhn1832004@gmail.com>
2024-10-28 22:56:18 +0700

    NguyenHoaiNam_20225653

---
+++ LAB02_20225653/Carts.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aimsproject;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Carts {
+    public static final int MAX_NUMBERS_ORDERS = 20;
+	private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERS];
+	int qtyOrdered ;
+	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered < MAX_NUMBERS_ORDERS) {
+            itemsOrdered[qtyOrdered] = disc;
+            qtyOrdered++;
+        }
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i] == disc) {
+                for (int j = i; j < qtyOrdered - 1; j++) {
+                    itemsOrdered[j] = itemsOrdered[j + 1];
+                }
+                itemsOrdered[qtyOrdered - 1] = null;
+                qtyOrdered--;
+                break;
+            }
+        }
+    }
+    
+    public void view(){
+        for (int i = 0; i < qtyOrdered; i++) {
+            System.out.println(itemsOrdered[i].getTitle());
+        }
+    }
+
+    public float totalCost() {
+        float total = 0.0f;
+        for (int i = 0; i < qtyOrdered; i++) {
+            total += itemsOrdered[i].getCost();
+        }
+        return total;
+    }
+}
